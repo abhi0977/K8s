94d39a2054eecfc82e2fa3287e655c128e9ce478
Abhi Ramani <abhiramani@Abhis-MacBook-Air.local>
2019-03-07 19:04:57 +0530

    Added something new

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ docker build -t abhi0977/dubai -f./dubai/Dockerfile ./dubai
 docker build -t abhi0977/paris -f ./paris/Dockerfile ./paris
 docker build -t abhi0977/london -f ./london/Dockerfile ./london
 
-docker push abhi0977/tokyo
-docker push abhi0977/delhi
-docker push abhi0977/dubai
-docker push abhi0977/paris
-docker push abhi0977/london
+# docker push abhi0977/tokyo
+# docker push abhi0977/delhi
+# docker push abhi0977/dubai
+# docker push abhi0977/paris
+# docker push abhi0977/london
 
 kubectl apply -f deployments
 
@@ -17,3 +17,29 @@ kubectl set image deployments/delhi-deployment delhi=abhi0977/delhi
 kubectl set image deployments/dubai-deployment dubai=abhi0977/dubai
 kubectl set image deployments/paris-deployment paris=abhi0977/paris
 kubectl set image deployments/london-deployment london=abhi0977/london
+
+
+
+        # \          SORRY            /
+        #  \                         /
+        #   \    This page does     /
+        #    ]   not exist yet.    [    ,'|
+        #    ]                     [   /  |
+        #    ]___               ___[ ,'   |
+        #    ]  ]\             /[  [ |:   |
+        #    ]  ] \           / [  [ |:   |
+        #    ]  ]  ]         [  [  [ |:   |
+        #    ]  ]  ]__     __[  [  [ |:   |
+        #    ]  ]  ] ]\ _ /[ [  [  [ |:   |
+        #    ]  ]  ] ] (#) [ [  [  [ :===='
+        #    ]  ]  ]_].nHn.[_[  [  [
+        #    ]  ]  ]  HHHHH. [  [  [
+        #    ]  ] /   `HH("N  \ [  [
+        #    ]__]/     HHH  "  \[__[
+        #    ]         NNN         [
+        #    ]         N/"         [
+        #    ]         N H         [
+        #   /          N            \
+        #  /           q,            \
+        # /                           \
+
